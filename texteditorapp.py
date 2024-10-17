422fecbee60794662932de18e79e97157d5fbbad
SATYAM KUMAR <satyamkumar@SATYAMs-MacBook-Air.local>
2024-10-17 08:46:44 +0530

    Testing

--- texteditorapp.py
+++ texteditorapp.py
@@ -4,6 +4,8 @@ from tkinter import filedialog, messagebox
 def new_file():
     text.delete(1.0, tk.END)
 
+
+
 def open_file():
     file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
     if file_path:
